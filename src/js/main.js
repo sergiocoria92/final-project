ffebf1c682ba7d50a34bb013caa6aee80cd48902
Sergio Coria <sergiocoria.my@gmail.com>
2025-06-20 12:24:06 -0600

    intento 1 de cargar todo

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
+// src/js/main.js
+import '../styles/header.css';
+import '../styles/footer.css';
+import '../styles/home.css';
+
 import { loadHeader } from './header.js';
 import { loadFooter } from './footer.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadHeader();
   loadFooter();
-
-})
+});
