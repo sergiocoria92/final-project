3f41323688c906de24287997a5329cbf1d261cdb
Sergio Coria <sergiocoria.my@gmail.com>
2025-06-20 16:31:58 -0600

    Deploy actualizado: ni idea

---
+++ assets/aboutme-BQQ9S8wl.js
@@ -0,0 +1,9 @@
+import"./main-CNjo6mIv.js";document.addEventListener("DOMContentLoaded",r);async function r(){const a=document.querySelector(".grid-container");if(a)try{const e=await(await fetch("/aboutme.json")).json();a.innerHTML="",e.forEach(t=>{const n=document.createElement("div");n.classList.add("photo-card"),n.innerHTML=`
+        <div class="caption">
+          <h4>${t.title}</h4>
+        </div>
+        <img src="${t.url}" alt="${t.title}" loading="lazy">
+        <div class="description">
+          <small>${t.description}</small>
+        </div>
+      `,a.appendChild(n)})}catch(o){console.error("Error loading event data:",o)}}
