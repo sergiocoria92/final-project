16c8f79b98b8661d95e33fe03544da71685986a9
Sergio Coria <sergiocoria.my@gmail.com>
2025-06-20 14:25:53 -0600

    4

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,21 @@
+
+/*styles lo que iba en MAIN*/
+
+// Este archivo es exclusivo para la página Home
+import '../styles/home.css';
+import '../styles/header.css';
+import '../styles/footer.css';
+
+import { loadHeader } from './header.js';
+import { loadFooter } from './footer.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadHeader();
+  loadFooter();
+});
+
+/*bottom*/
+
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("booking-form");
   if (form) {
